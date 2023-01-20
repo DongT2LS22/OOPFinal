7627abb2d8410e1fb6253645ce2ac5206667552b
DongT2LS <vanhein445566@gmail.com>
2023-01-20 20:34:58 +0700

    view Update 2 20/1

--- src/main/java/hust/vietnamesehistory/controller/vietnamesehistory/PeopleDetailController.java
+++ src/main/java/hust/vietnamesehistory/controller/vietnamesehistory/PeopleDetailController.java
@@ -1,4 +1,22 @@
 package hust.vietnamesehistory.controller.vietnamesehistory;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PeopleDetailController {
+    @FXML
+    public void backScene(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("people.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
